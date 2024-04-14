d7b4f3155655010fb3844a6515dad6aa755fba72
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-04-14 17:12:11 +0300

    Completed Formik and Form Validation

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
+
 export const SignupForm = () => {
   const [customers, setCustomers] = useState([{}]);
   const [refreshPage, setRefreshPage] = useState(false);
@@ -53,7 +54,6 @@ export const SignupForm = () => {
 
   return (
     <div>
-      <h1>Customer sign up form</h1>
       <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
         <label htmlFor="email">Email Address</label>
         <br />
